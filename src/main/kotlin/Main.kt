7b0b86c33136134d7fc066264b2b083f2bcbbf7f
Rob Dearling <rob@nulldev.uk>
2024-12-11 18:42:05 +0000

    feat: Basic implementation in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 package org.bytecodeprobe
 
+import java.nio.file.Path
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args: Array<String>) {
-    val pathToJar = args[0]
-
+    val pathToJar = Path.of(args[0])
+    val jar = JarWrapper(pathToJar)
+    println(jar.classes.size)
 }
